b3b3df6273db0d65702a2a86b4ff2eb8ffad8277
Eric Delord <04-ete.elit@icloud.com>
2022-08-25 16:27:31 +0200

    added autofill login page

---
+++ app/Filament/Pages/Auth/Login.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Filament\Pages\Auth;
+
+use Filament\Http\Livewire\Auth\Login as BasePage;
+
+class Login extends BasePage
+{
+    public function mount(): void
+    {
+        parent::mount();
+
+        $this->form->fill([
+            'email' => 'test@example.com',
+            'password' => 'password',
+            'remember' => true,
+        ]);
+    }
+}
